4b16275b5ed1ae6eb024d9c7d5a3dbf6a2bea7ad
mikedevpax <michael.tellmann@pax.us>
2018-11-30 10:38:33 -0500

    Here is a basic menu

--- WebAPIDemo/src/Mainapp.java
+++ WebAPIDemo/src/Mainapp.java
@@ -19,39 +19,44 @@ public class Mainapp {
 		}
 		else if (choice ==2) {
 			// call the function to List all of the resellers
+			//choose a reseller
+			
+			//Create a function to Display the information for the Reseller and provide the option to Modify or Delete
+			int resellerChoice = 1;
+			if (resellerChoice == 1 ) {
+				//Third screen will display options for a particular Reseller
+				
+				//Print the name of the Reseller at the top followed by:
+				System.out.println("\nThese are the options for Test Reseller");//DELETE THIS LINE LATER
+				System.out.println("1. Modify Reseller information");
+				System.out.println("2. Delete Reseller");
+				//Fourth screen will either show the data that can be modified for the chosen Reseller for 1, 
+				//or show a confirmation page if the choice was 2.
+				int choice2 = scan.nextInt();
+				if (choice2 == 1) {
+					//Display the information you can change for a Reseller, and what format you need to type to change the Reseller data
+				}
+				else if (choice2 == 2) {
+					System.out.println("Are you sure? Y/N");
+					String delChoice = scan.next();
+					if(delChoice.equals("Y") || delChoice.equals("y")) {
+						//Call function to delete a Reseller
+					}
+					else if (delChoice.equals("N") || delChoice.equals("n")) {	
+						//Return back to Reseller page
+					}
+					else {
+						System.out.println("Invalid Entry");
+					}
+				}
+			}
 		}
 		else {
 			System.out.println("Invalid Entry");
 		}
+		scan.close();
+	
 		
-		//Third screen will display options for a particular Reseller
-		
-		//Print the name of the Reseller at the top followed by:
-		System.out.println("1. Modify Reseller information");
-		System.out.println("2. Delete Reseller");
-
-		
-		//Fourth screen will either show the data that can be modified for the chosen Reseller for 1, 
-		//or show a confirmation page if the choice was 2.
-		int choice2 = scan.nextInt();
-		if (choice2 == 1) {
-			//Display the information you can change for a Reseller, and what format you need to type to change the Reseller data
-		}
-		else if (choice2 == 2) {
-			Scanner del = new Scanner(System.in); 
-			System.out.println("Are you sure? Y/N");
-			String delChoice = del.next();
-			
-			if(delChoice == "Y" || delChoice == "y") {
-				//Call function to delete a Reseller
-			}
-			else if (delChoice =="N" || delChoice== "n") {
-				//Return back to Reseller page
-			}
-			else {
-				System.out.println("Invalid input");
-			}
-		}
 		
 	}
 
