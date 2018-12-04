a596a77c6c66769eadd1b1dd6d2d4d6bb1c5f310
Praveen-Patchala <praveen.patchala@pax.us>
2018-12-04 18:02:36 -0500

    Modified Helper Class

--- src/main/java/Helper.java
+++ src/main/java/Helper.java
@@ -7,7 +7,7 @@ public class Helper {
     static String systemAccessSecret="V4XTQ5DADRRC88GMZ97ETH5E47E5AHIUGH1LCPPL";
     static String url="https://www.paxstores.com/p-market-api";
 
-    static void print(Result<ResellerDTO> result){
+    static void printResult(Result<ResellerDTO> result){
         ResellerDTO data=result.getData();
         System.out.println("ID:"+data.getId()+"\nName:"+data.getName()+"\nAddress:"+data.getAddress()+"\nCompany"+data.getCompany()+"\nContact:"+data.getContact()+"\nCountry"+data.getCountry()+"\nEmail"+data.getEmail()+"\nPhone"+data.getPhone()+"\nPostCode:"+data.getPostcode()+"\nStatus"+data.getStatus()+"\nEntity Attribute Values:"+data.getEntityAttributeValues());
     }
